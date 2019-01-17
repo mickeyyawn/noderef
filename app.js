064d0fec9b04fef0b855e22f85b28f4d09f163ef
Mickey Yawn <mickeyyawn@gmail.com>
2019-01-17 10:07:01 -0500

    updated packages

--- app.js
+++ app.js
@@ -51,6 +51,36 @@ routes = require('./routes');
 
 logs.info(app.locals.info);
 
+
+const exerciseRedis = () => {
+
+    var redis = require("redis"),
+    client = redis.createClient();
+    // hash / some key / some value  e.g. a hash in the redis map can have multiple keys...
+    client.hset("hash key", "hashtest 1", "some value", redis.print);
+    client.hset("hash key", "a different key at the same hash location", "some new value", redis.print);
+
+
+    // test to see if hash/key exists...
+
+    client.hexists("hash key", "hashtest 1", (err, data) => {
+      if (data == 1) {
+          console.log('hash/key exists...')
+      }
+      else {
+        console.log('hash/key DOES NOT exist...')
+      }
+      //
+      // let's destroy the client conn
+      //
+      client.quit();
+    });
+
+}
+
+exerciseRedis();
+
+
 process.on('uncaughtException', err => {
     logs.error('something went wrong!', err);
 
